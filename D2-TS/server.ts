05f6c15b6529c43880b8a6ec895c49386acf521f
ravi kumar dhawan <ravidhawan8899@gmail.com>
2022-12-06 01:18:10 +0530

    os module

--- D2-TS/server.ts
+++ D2-TS/server.ts
@@ -1,4 +1,6 @@
 import http, { IncomingMessage, Server, ServerResponse } from 'http'
+// const os = require('os');
+import os from 'os'
 const hostname:string = '127.0.0.1';
 const port:number = 5000;
 
@@ -6,10 +8,18 @@ const port:number = 5000;
 const server:Server = http.createServer((req:IncomingMessage,res:ServerResponse)=>{
     res.statusCode = 200;
     res.setHeader('content-type','text/html');
-res.end(`<h3 style = " color:red;font-size:55px  " > hello welcome to ts tut.  </h3>`)
-})
+// res.end(`hello welcome to ts tut.`)
+// os module
+let osData = {
+    totalMemory : os.totalmem(),
+    freeMemory:os.freemem(),
+    homedir:os.homedir(),
+    computerName:os.hostname()
 
-server.listen(port,()=>{
+}
+res.end(JSON.stringify(osData))
+})
+server.listen(port,hostname,()=>{
  console.log(`server is running at  http://${hostname}:${port}` )
 }
 )
